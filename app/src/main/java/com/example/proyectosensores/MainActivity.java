27668ca5463d7760bd5eae5a25743f350575659a
yisel723 <66075148+yisel723@users.noreply.github.com>
2020-12-10 14:46:02 -0500

    Commit

--- app/src/main/java/com/example/proyectosensores/MainActivity.java
+++ app/src/main/java/com/example/proyectosensores/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.proyectosensores;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //HOLAAAA
+
+
+
+
     }
 }
