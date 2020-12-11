9d27a10192787d195c3498290b488ff563558cd8
Nazareth08 <Nazareth.Navarrete@utp.ac.pa>
2020-12-10 19:46:42 -0500

    boton de aproximacion

--- app/src/main/java/com/example/proyectosensores/MainActivity.java
+++ app/src/main/java/com/example/proyectosensores/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.proyectosensores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
@@ -13,9 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //HOLAAAA
-
 //ya estan los cambios hechos
+    }
+    //pasar del activity principal al activity de sensor luz
+    public void SensorLuz (View view){
+        Intent i = new Intent(this,SensorLuz.class);
+        startActivity(i);
 
+    }
+    public void SensorAproximacion (View view){
+        Intent i = new Intent(this,SensorAproximacion.class);
+        startActivity(i);
 
+    }
+    public void Gps (View view){
+        Intent i = new Intent(this,Gps.class);
+        startActivity(i);
     }
 }
