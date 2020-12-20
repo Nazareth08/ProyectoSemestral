0c064b46e18d6d349ca4e46cdd30f115eaca6df0
Sensores2020 <Nazareth.Navarrete@utp.ac.pa>
2020-12-19 19:00:48 -0500

    CAMBIO PROBANDO

--- app/src/main/java/com/example/proyectosensores/MainActivity.java
+++ app/src/main/java/com/example/proyectosensores/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+// PROBANDOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
     }
 
     //pasar del activity principal al activity acelerometro
